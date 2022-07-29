a8e6a09a11a534240a2ea6b6f43f1a4edf39f158
krdsploit <104392085+krdsploit@users.noreply.github.com>
2022-07-29 05:01:47 -0700

    Add files via upload

---
+++ sendingmail.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+
+    <title>Sending Mail </title>
+
+
+    <head>
+
+
+    <body>
+
+
+
+
+
+<?php
+
+
+    echo "Welcome Buddy";
+
+    $mailaddress = "enter_email_address";
+    $mailsubject = "enter_your_sub";
+    $mailbody = "Write Ya Msg";
+    mail($mailaddress,$mailsubject,$mailbody)
+
+
+
+?>
+
+
+</html>
+</body>
